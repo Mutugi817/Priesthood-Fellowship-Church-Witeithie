968312cbb457a7f0a4440dbdbe0e3c580ce56623
Brian Mutugi <gakenge7@gmail.com>
2025-09-20 13:04:14 +0300

    Updated the september 21st Sunday posters

--- client/assets/assets.js
+++ client/assets/assets.js
@@ -1,13 +1,9 @@
 import logo from './Pristhood-logo (2)-Photoroom.png';
-import header_bg_image from './Header_bg.png';
-import worship from './WENO.jpg';
-import sunday from './Sunday.jpg';
-import bible_study from './BibleStudy1.jpg';
+import sunday from './21stsunday.jpg';
+import bible_study from './bible study Template (1) - Made with PosterMyWall (1).jpg';
 
 export const assets = {
     logo,
-    header_bg_image,
     sunday,
-    worship,
     bible_study
 }
--- client/src/components/Updates/Updates.jsx
+++ client/src/components/Updates/Updates.jsx
@@ -6,9 +6,6 @@ function Updates() {
     <div className="updates">
       <h2>Upcoming Events</h2>
       <div className="image">
-        <img src={assets.worship} alt="Bible study" />
-      </div>
-      <div className="image">
         <img src={assets.bible_study} alt="Bible study" />
       </div>
       <div className="image">
